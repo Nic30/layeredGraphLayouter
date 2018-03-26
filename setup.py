44cd24cd74cc1afaeb3af8606cc791b2b15730cd
Nic30 <nic30@seznam.cz>
2018-03-26 23:35:01 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(name='layeredGraphLayouter',
+      version='0.1',
+      description='framework for automatic drawing of layered graphs',
+      url='https://github.com/Nic30/layeredGraphLayouter',
+      author='Michal Orsak',
+      author_email='michal.o.socials@gmail.com',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
